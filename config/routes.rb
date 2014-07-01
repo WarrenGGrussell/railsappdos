fbfb9f07e755016291bbccef46d1b153e3a37811
WarrenG <warrencoke@gmail.com>
2014-07-01 03:55:53 +0000

    level 13

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
 
   get 'users/create'
 
-  resources :movies
+  resources :movies do 
+    resources :reviews, only: [:new, :create]
+  end
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
   # The priority is based upon order of creation: first created -> highest priority.
